7bbd5757e734775072f24397f76c34b4f6fcd027
Juan Pablo Lopez <juanpa9951@gmail.com>
2024-02-09 13:56:43 +0100

    new version

---
+++ pca9685/pca9685.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// The PCA9685 address
+#define PCA9685_ADDRESS 0x40
+
+// Create the PWM servo driver object
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(PCA9685_ADDRESS);
+
+// Define constants for the servo
+#define SERVO_PIN 0     // Servo connected to channel 0 of PCA9685
+#define SERVO_MIN_PULSE_WIDTH 100   // Minimum pulse width in microseconds 100    50
+#define SERVO_MAX_PULSE_WIDTH 500   // Maximum pulse width in microseconds 500   500
+#define SERVO_MIN_ANGLE 0            // Minimum angle in degrees
+#define SERVO_MAX_ANGLE 180          // Maximum angle in degrees
+
+void setup() {
+  Serial.begin(115200);
+  pwm.begin();
+  pwm.setPWMFreq(50);  // Analog servos typically run at 50Hz - 60Hz updates
+
+  delay(10);
+}
+
+void loop() {
+  // Move the servo to different positions
+  for (int angle = SERVO_MIN_ANGLE; angle <= SERVO_MAX_ANGLE; angle += 10) {
+    setServoAngle(SERVO_PIN, angle);
+    setServoAngle(9, angle);
+    delay(500); // Delay to allow servo to reach the desired position
+  }
+}
+
+// Function to set the angle of the servo
+void setServoAngle(uint8_t servoNum, int angle) {
+  // Convert angle to pulse width
+  uint16_t pulse_width = map(angle, 0, 180, SERVO_MIN_PULSE_WIDTH, SERVO_MAX_PULSE_WIDTH);
+  // Set pulse width
+  pwm.setPWM(servoNum, 0, pulse_width);
+}
---
+++ servo_spiner/servo_spiner.ino
@@ -0,0 +1,67 @@
+#include <Servo.h>
+
+Servo servo1;  // Define servo1 object
+Servo servo2;  // Define servo2 object
+
+Servo servo3;  // Define servo1 object
+Servo servo4;  // Define servo2 object
+
+int servo1Pin = 10;  // PWM pin for servo1   Black
+int servo2Pin = 11; // PWM pin for servo2    Blue
+
+int servo3Pin = 5;  // PWM pin for servo3    Black
+int servo4Pin = 6; // PWM pin for servo4     Blue
+
+int t1 = 80;
+int t2 = 100;
+
+void setup() {
+  servo1.attach(servo1Pin); // Attach servo1 to its pin
+  servo2.attach(servo2Pin); // Attach servo2 to its pin
+  servo3.attach(servo3Pin); // Attach servo1 to its pin
+  servo4.attach(servo4Pin); // Attach servo2 to its pin
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // scale it for use with the servo (value between 0 and 180)
+  digitalWrite(13,HIGH);
+  servo2.write(0);  
+  delay(t2);
+  servo2.write(20);  
+  delay(t2);
+  servo2.write(40);  
+  delay(t2);
+  servo2.write(60);  
+  delay(t2);
+  servo2.write(80);  
+  delay(t2); 
+  servo2.write(100);  
+  delay(t2);
+  servo2.write(120);  
+  delay(t2);
+  servo2.write(140);  
+  delay(t2);
+  servo2.write(160);  
+  delay(t2);
+  servo2.write(180);  
+  delay(t2);
+  servo2.write(160);  
+  delay(t2);
+  servo2.write(140);  
+  delay(t2);
+  servo2.write(120);  
+  delay(t2);
+  servo2.write(100);  
+  delay(t2);
+  servo2.write(80);  
+  delay(t2); 
+  servo2.write(60);  
+  delay(t2);
+  servo2.write(40);  
+  delay(t2);
+  servo2.write(20);  
+  delay(t2);
+  servo2.write(0);  
+  delay(t2);  
+}
