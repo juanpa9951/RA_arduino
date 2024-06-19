96da703f84c3159c13f9de92f6c26a5fdf88a5c9
Juan Pablo Lopez <juanpa9951@gmail.com>
2024-06-19 16:16:49 +0200

    new version

--- IR_remote_2/IR_remote_2.ino
+++ IR_remote_2/IR_remote_2.ino
@@ -19,9 +19,10 @@ void setup() {
  pinMode(4,OUTPUT);
  pinMode(5,OUTPUT);
  pinMode(6,OUTPUT);
- // pinMode(6,OUTPUT);
- // pinMode(7,OUTPUT);
- // pinMode(8,OUTPUT);
+ pinMode(7,OUTPUT);
+ pinMode(8,OUTPUT);
+ pinMode(9,OUTPUT);
+ pinMode(10,OUTPUT);
 }
 
 void loop() {
